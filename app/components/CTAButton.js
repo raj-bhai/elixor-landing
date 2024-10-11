09edf0fbbf3d312d2aa6c1491ca02c2c039a54d9
Jatin-ge <Jatin@growthescalators.com>
2024-10-11 14:46:47 +0530

    Update CTAButton.js

--- app/components/CTAButton.js
+++ app/components/CTAButton.js
@@ -5,7 +5,7 @@ export default function CTAButton({ text, onClick, subtext }) {
     <div className="flex font-sans flex-col items-center mt-12">
       <button
         onClick={() => {
-            window.open('https://calendly.com/elixzor-yta/youtube-discovery-call-clone');
+            window.open('https://calendly.com/paul-elixzorconsulting/yta-cohort-call');
         }}
         className="bg-gradient-to-r from-[#6B03B0] to-[#270049] hover:from-[#A32CC4] hover:to-[#600080] text-white font-bold py-4 px-8 sm:px-10 md:px-12 rounded-full text-base sm:text-lg md:text-2xl shadow-lg transition"
       >
